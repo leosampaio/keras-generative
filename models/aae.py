6cbf5db379c9e43b5fac402d229f058ca15823d9
alex <homaralex@gmail.com>
2017-11-27 13:24:35 +0100

    Move reconstruction callback to utils

--- models/aae.py
+++ models/aae.py
@@ -1,95 +1,12 @@
 import argparse
-import os
 import h5py
 from keras import Model, Input
-from keras.callbacks import Callback
 from keras.layers import Flatten, Dense, BatchNormalization, Activation, Reshape
 
 from models.layers import BasicConvLayer, BasicDeconvLayer
-import matplotlib.pyplot as plt
 import numpy as np
 
-
-def slerp(val, low, high):
-    """Spherical interpolation. val has a range of 0 to 1."""
-    if val <= 0:
-        return low
-    elif val >= 1:
-        return high
-    elif np.allclose(low, high):
-        return low
-    omega = np.arccos(np.dot(low / np.linalg.norm(low), high / np.linalg.norm(high)))
-    so = np.sin(omega)
-    return np.sin((1.0 - val) * omega) / so * low + np.sin(val * omega) / so * high
-
-
-def interpolate_vectors(a, b, steps=10):
-    interp = np.zeros((steps,) + a.shape)
-    for i in range(a.shape[0]):
-        for step in range(steps - 1):
-            interp[step][i] = a[i] + (b[i] - a[i]) / steps * step
-    interp[steps - 1] = b
-    return interp
-
-
-class PlotReconstructions(Callback):
-    def __init__(self, encoder, decoder, z_dim=256, examples_to_plot=10):
-        super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-        self.examples_to_plot = examples_to_plot
-        self.z_dim = z_dim
-
-        if not os.path.isdir('aae'):
-            os.makedirs('aae')
-
-    def on_epoch_end(self, epoch, logs=None):
-        data = self.validation_data[0][:self.examples_to_plot]
-        decoded_imgs = self.model.predict(data)
-        random_z = np.random.normal(size=(self.examples_to_plot, self.z_dim))
-        random_imgs = self.decoder.predict(random_z)
-
-        # interpolations
-        a = data[0]
-        b = data[1]
-        encodings = self.encoder.predict(np.array([a, b]).reshape((2,) + a.shape))
-        interp = interpolate_vectors(encodings[0], encodings[1])
-        decoded_interp = self.decoder.predict(interp)
-
-        plot_reconstructions(data, decoded_imgs, random_imgs, decoded_interp, 'aae/aae_{}.png'.format(epoch))
-
-
-def plot_reconstructions(x, y, random_imgs, interpolations, plotname):
-    n = x.shape[0]
-    img_size = x.shape[1]
-    plt.figure(figsize=(15, 4))
-    for i in range(n):
-        # display original
-        ax = plt.subplot(4, n, i + 1)
-        plt.imshow(x[i].reshape((img_size, img_size, 3)))
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        # display reconstruction
-        ax = plt.subplot(4, n, i + 1 + n)
-        plt.imshow(y[i].reshape((img_size, img_size, 3)))
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        # display random images
-        ax = plt.subplot(4, n, i + 1 + 2 * n)
-        plt.imshow(random_imgs[i].reshape((img_size, img_size, 3)))
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-        # interpolation
-        ax = plt.subplot(4, n, i + 1 + 3 * n)
-        plt.imshow(interpolations[i].reshape((img_size, img_size, 3)))
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
-
-    plt.savefig(plotname)
-    plt.close()
+from models.utils import PlotReconstructions
 
 
 def load_data(train_cnt=10000, val_cnt=500):
--- models/utils.py
+++ models/utils.py
@@ -1,4 +1,9 @@
 from keras import backend as K
+from keras.callbacks import Callback
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def set_trainable(model, train):
     """
@@ -11,3 +16,85 @@ def set_trainable(model, train):
 
 def zero_loss(y_true, y_pred):
     return K.zeros_like(y_true)
+
+
+def slerp(val, low, high):
+    """Spherical interpolation. val has a range of 0 to 1."""
+    if val <= 0:
+        return low
+    elif val >= 1:
+        return high
+    elif np.allclose(low, high):
+        return low
+    omega = np.arccos(np.dot(low / np.linalg.norm(low), high / np.linalg.norm(high)))
+    so = np.sin(omega)
+    return np.sin((1.0 - val) * omega) / so * low + np.sin(val * omega) / so * high
+
+
+def interpolate_vectors(a, b, steps=10):
+    interp = np.zeros((steps,) + a.shape)
+    for i in range(a.shape[0]):
+        for step in range(steps - 1):
+            interp[step][i] = a[i] + (b[i] - a[i]) / steps * step
+    interp[steps - 1] = b
+    return interp
+
+
+class PlotReconstructions(Callback):
+    def __init__(self, encoder, decoder, z_dim=256, examples_to_plot=10):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.examples_to_plot = examples_to_plot
+        self.z_dim = z_dim
+
+        if not os.path.isdir('aae'):
+            os.makedirs('aae')
+
+    def on_epoch_end(self, epoch, logs=None):
+        data = self.validation_data[0][:self.examples_to_plot]
+        decoded_imgs = self.model.predict(data)
+        random_z = np.random.normal(size=(self.examples_to_plot, self.z_dim))
+        random_imgs = self.decoder.predict(random_z)
+
+        # interpolations
+        a = data[0]
+        b = data[1]
+        encodings = self.encoder.predict(np.array([a, b]).reshape((2,) + a.shape))
+        interp = interpolate_vectors(encodings[0], encodings[1])
+        decoded_interp = self.decoder.predict(interp)
+
+        plot_reconstructions(data, decoded_imgs, random_imgs, decoded_interp, 'aae/aae_{}.png'.format(epoch))
+
+
+def plot_reconstructions(x, y, random_imgs, interpolations, plotname):
+    n = x.shape[0]
+    img_size = x.shape[1]
+    plt.figure(figsize=(15, 4))
+    for i in range(n):
+        # display original
+        ax = plt.subplot(4, n, i + 1)
+        plt.imshow(x[i].reshape((img_size, img_size, 3)))
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(4, n, i + 1 + n)
+        plt.imshow(y[i].reshape((img_size, img_size, 3)))
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display random images
+        ax = plt.subplot(4, n, i + 1 + 2 * n)
+        plt.imshow(random_imgs[i].reshape((img_size, img_size, 3)))
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # interpolation
+        ax = plt.subplot(4, n, i + 1 + 3 * n)
+        plt.imshow(interpolations[i].reshape((img_size, img_size, 3)))
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+    plt.savefig(plotname)
+    plt.close()
