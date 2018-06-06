New metrics, fixes to metric computation threading

models/base.py
@@ -74,6 +74,7 @@ class BaseModel(metaclass=ABCMeta):
                 self.metric_calculators = {m: getattr(self, "calculate_{}".format(m)) for m in metrics}
                 self.metrics = {m: [] for m in metrics}
                 self.metrics_calc_threads = {m: threading.Thread() for m in metrics}
+                self.metric_types = {m: getattr(self, "{}_metric_type".format(m)) for m in metrics}
                 for t in self.metrics_calc_threads.values():
                     t.start()
             except AttributeError:
@@ -90,7 +91,7 @@ class BaseModel(metaclass=ABCMeta):
         return self.get_experiment_id()
     experiment_id = property(_get_experiment_id)
 
-    def main_loop(self, dataset, samples, samples_conditionals=None, epochs=100, batchsize=100):
+    def main_loop(self, dataset, samples, epochs=100, batchsize=100):
         '''
         Main learning loop
         '''
@@ -101,19 +102,21 @@ class BaseModel(metaclass=ABCMeta):
         if not os.path.isdir(out_dir):
             os.mkdir(out_dir)
 
-        res_out_dir = os.path.join(out_dir, 'results')
-        if not os.path.isdir(res_out_dir):
-            os.mkdir(res_out_dir)
+        self.res_out_dir = os.path.join(out_dir, 'results')
+        if not os.path.isdir(self.res_out_dir):
+            os.mkdir(self.res_out_dir)
 
-        wgt_out_dir = os.path.join(out_dir, 'weights')
-        if not os.path.isdir(wgt_out_dir):
-            os.mkdir(wgt_out_dir)
+        self.wgt_out_dir = os.path.join(out_dir, 'weights')
+        if not os.path.isdir(self.wgt_out_dir):
+            os.mkdir(self.wgt_out_dir)
 
         # Start training
         print('\n\n--- START TRAINING ---\n')
         num_data = len(dataset)
         self.dataset = dataset
+        self.samples = samples
         self.g_losses, self.d_losses, self.losses_ratio = [], [], []
+        self.make_predict_functions()
         for e in range(self.last_epoch, epochs):
             perm = np.random.permutation(num_data)
             start_time = time.time()
@@ -152,37 +155,12 @@ class BaseModel(metaclass=ABCMeta):
                     return
 
             # plot samples and losses and send notification if it's checkpoint time
-            is_checkpoint = ((self.current_epoch) % self.checkpoint_every) == 0
-            is_notification_checkpoint = ((self.current_epoch) % self.notify_every) == 0
-            is_metrics_checkpoint = ((self.current_epoch) % self.metrics_every) == 0
-            outfile = None
-            if is_checkpoint:
-                outfile = os.path.join(res_out_dir, "epoch_{:04}_batch_{}".format(self.current_epoch, b + bsize))
-                self.save_images(samples, "{}_samples.png".format(outfile), conditionals_for_samples=samples_conditionals)
-                self.save_model(wgt_out_dir, self.current_epoch)
-                self.plot_losses_hist("{}_losses.png".format(outfile))
-            if is_notification_checkpoint:
-                if not outfile:
-                    outfile = os.path.join(res_out_dir, "epoch_{:04}_batch_{}".format(self.current_epoch, b + bsize))
-                    self.save_images(samples, "{}_samples.png".format(outfile), conditionals_for_samples=samples_conditionals)
-                    self.plot_losses_hist("{}_losses.png".format(outfile))
-                try:
-                    message = "[{}] Epoch #{:04}".format(self.experiment_id, self.current_epoch)
-                    notify_with_image("{}_samples.png".format(outfile), experiment_id=self.experiment_id, message=message)
-                    notify_with_image("{}_losses.png".format(outfile), experiment_id=self.experiment_id, message=message)
-                except NameError as e:
-                    print(e)
-            if is_metrics_checkpoint:
-                outfile = os.path.join(res_out_dir, "epoch_{:04}_batch_{}_metrics.png".format(self.current_epoch, b + bsize))
-                log_message = self.calculate_all_metrics()
-                self.plot_all_metrics(outfile)
-                print(log_message)
-                try:
-                    message = "[{}] Epoch #{:04}".format(self.experiment_id, self.current_epoch)
-                    notify_with_image(outfile, experiment_id=self.experiment_id, message=message)
-                    notify_with_message(log_message, self.experiment_id)
-                except NameError as e:
-                    print(e)
+            if ((self.current_epoch) % self.checkpoint_every) == 0:
+                self.save_model(self.wgt_out_dir, self.current_epoch)
+            if ((self.current_epoch) % self.notify_every) == 0:
+                self.send_checkpoint_notification()
+            if ((self.current_epoch) % self.metrics_every) == 0:
+                self.send_metrics_notification()
 
             elapsed_time = time.time() - start_time
             print('Took: {}s\n'.format(elapsed_time))
@@ -200,6 +178,13 @@ class BaseModel(metaclass=ABCMeta):
         self.d_losses.append(losses['d_loss'])
         self.losses_ratio.append(losses['g_loss'] / losses['d_loss'])
 
+    def make_predict_functions(self):
+        """
+        implement `_make_predict_function()` calls here if you plan on making
+        predictions on multiple threads
+        """
+        pass
+
     def make_batch(self, dataset, indx):
         '''
         Get batch from dataset
@@ -210,32 +195,6 @@ class BaseModel(metaclass=ABCMeta):
 
     def did_collapse(self, losses):
         return False
-
-    def save_images(self, samples, filename, conditionals_for_samples=None):
-        '''
-        Save images generated from random sample numbers
-        '''
-        imgs = self.predict(samples)
-        # imgs = np.clip(imgs * 0.5 + 0.5, 0.0, 1.0)
-        if imgs.shape[3] == 1:
-            imgs = np.squeeze(imgs, axis=(3,))
-
-        self.save_image_as_plot(imgs, filename)
-
-    def save_image_as_plot(self, imgs, filename):
-        fig = plt.figure(figsize=(8, 8))
-        grid = gridspec.GridSpec(10, 10, wspace=0.1, hspace=0.1)
-        for i in range(100):
-            ax = plt.Subplot(fig, grid[i])
-            if imgs.ndim == 4:
-                ax.imshow(imgs[i, :, :, :], interpolation='none', vmin=0.0, vmax=1.0)
-            else:
-                ax.imshow(imgs[i, :, :], cmap='gray', interpolation='none', vmin=0.0, vmax=1.0)
-            ax.axis('off')
-            fig.add_subplot(ax)
-
-        fig.savefig(filename, dpi=200)
-        plt.close(fig)
 
     def save_model(self, out_dir, epoch):
         folder = os.path.join(out_dir, 'epoch_%05d' % epoch)
@@ -284,7 +243,7 @@ class BaseModel(metaclass=ABCMeta):
 
     def predict_images(self, z_sample):
         images = self.predict(z_sample)
-        if images.shape[3] == 1:
+        if images.shape[3] == 1:  
             images = np.squeeze(imgs, axis=(3,))
         # images = np.clip(predictions * 0.5 + 0.5, 0.0, 1.0)
         return images
@@ -305,31 +264,96 @@ class BaseModel(metaclass=ABCMeta):
 
     def calculate_all_metrics(self):
         log_message = "Metrics for Epoch #{}: ".format(np.max((self.current_epoch - self.metrics_every, 1)))
+        self.did_go_through_all_metrics = threading.Event()
         for m, calculation_fun in self.metric_calculators.items():
             self.metrics_calc_threads[m].join()
             if not self.metrics[m]:
                 log_message = "{} {}: {},".format(log_message, m, "waiting")
-            else:
+            elif self.metric_types[m] == 'lines':
                 log_message = "{} {}: {},".format(log_message, m, self.metrics[m][-1])
+            else:
+                log_message = "{} {}: plotted,".format(log_message, m)
             finished_cgraph_use_event = threading.Event()
+            
             self.metrics_calc_threads[m] = threading.Thread(target=self.metrics_calculation_worker,
                                                             args=(calculation_fun, self.metrics[m],
-                                                                  finished_cgraph_use_event))
+                                                                  self.metric_types[m],
+                                                                  finished_cgraph_use_event,
+                                                                  self.did_go_through_all_metrics))
             self.metrics_calc_threads[m].start()
             finished_cgraph_use_event.wait()
         return log_message
 
-    def metrics_calculation_worker(self, calculation_fun, results,
-                                   finished_cgraph_use_event):
+    def metrics_calculation_worker(self, calculation_fun, results, mtype,
+                                   finished_cgraph_use_event, did_go_through_all_metrics):
         result = calculation_fun(finished_cgraph_use_event)
-        results.append(result)
+        # wait for all metrics to finish 
+        # before appending the results
+        did_go_through_all_metrics.wait()
+        if mtype == 'lines':
+            results.append(result)
+        elif mtype == 'scatter' or mtype == 'image-grid':
+            if not results:
+                results.append(result)
+            else:
+                results[-1] = result
+
+    def calculate_samples(self, finished_cgraph_use_event):
+        imgs = self.predict(self.samples)
+        finished_cgraph_use_event.set()
+        if imgs.shape[3] == 1:
+            imgs = np.squeeze(imgs, axis=(3,))
+        return imgs
+    samples_metric_type = 'image-grid'
 
     def plot_all_metrics(self, outfile):
         if all(d for d in self.metrics.values()):
+            e_metrics, e_iters, e_names, e_types, e_legend = self.get_extra_metrics_for_plot()
+            metrics, names = list(self.metrics.values()), list(self.metrics.keys())
+            iters = list(range(self.metrics_every, self.current_epoch, self.metrics_every))
+            types = list(self.metric_types[k] for k in self.metrics.keys())
             plot_metrics(outfile,
-                         metrics_list=list(self.metrics.values()),
-                         iterations_list=list(range(len(next(iter(self.metrics.values()))))),
-                         metric_names=list(self.metrics.keys()),
-                         legend=[True] * len(self.metrics),
+                         metrics_list=metrics+e_metrics,
+                         iterations_list=iters+e_iters,
+                         metric_names=names+e_names,
+                         types=types+e_types,
+                         legend=([True]*len(self.metrics))+e_legend,
                          figsize=8,
-                         wspace=0.5)
+                         wspace=0.15)
+            self.did_go_through_all_metrics.set()
+            return True
+        else:
+            self.did_go_through_all_metrics.set()
+            return False
+
+    def get_extra_metrics_for_plot(self):
+        """
+        should return any metrics you want to plot together with the 
+        frequent ones
+
+        returns 5 lists: 
+            metrics_list, iterations_list, metric_names, metric_types, use_legend_list
+        """
+        return []*5
+
+    def send_checkpoint_notification(self):
+        outfile_signature = os.path.join(self.res_out_dir, "epoch_{:04}".format(self.current_epoch))
+        self.plot_losses_hist("{}_losses.png".format(outfile_signature))
+        try:
+            message = "[{}] Epoch #{:04}".format(self.experiment_id, self.current_epoch)
+            notify_with_image("{}_losses.png".format(outfile_signature), experiment_id=self.experiment_id, message=message)
+        except NameError as e:
+            print(e)
+
+    def send_metrics_notification(self):
+        outfile = os.path.join(self.res_out_dir, "epoch_{:04}_metrics.png".format(self.current_epoch))
+        log_message = self.calculate_all_metrics()
+        did_plot = self.plot_all_metrics(outfile)
+        print(log_message)
+        try:
+            message = "[{}] Epoch #{:04}".format(self.experiment_id, self.current_epoch)
+            notify_with_message(log_message, self.experiment_id)
+            if did_plot:
+                notify_with_image(outfile, experiment_id=self.experiment_id, message=message)
+        except NameError as e:
+            print(e)

models/topgan.py
@@ -5,9 +5,11 @@ from pprint import pprint
 
 import numpy as np
 from sklearn.svm import LinearSVC
-from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.manifold import TSNE
 
 import keras
 from keras.engine.topology import Layer
@@ -25,12 +27,15 @@ from .base import BaseModel
 
 from .utils import *
 from .layers import *
+from .metrics import *
+from . import mmd
 
 try:
     from .notifyier import *
 except ImportError as e:
     print(e)
     print("You did not set a notifyier. Notifications will not be sent anywhere")
+
 
 def triplet_lossfun_creator(margin=1., zdims=256, inverted=False):
     def triplet_lossfun(_, y_pred):
@@ -80,6 +85,10 @@ def generator_lossfun(y_true, y_pred):
 
     return q_error + p_error
 
+def mmd_lossfun(y_true, y_pred):
+    x = K.batch_flatten(y_true)
+    x_hat = K.batch_flatten(y_pred)
+    return tf.log(mmd.rbf_mmd2(x, x_hat))
 
 class TOPGAN(BaseModel, metaclass=ABCMeta):
 
@@ -410,7 +419,10 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
             self.loss_control = dict(list(zip(self.loss_names, ['none'] * self.n_losses)))
             self.loss_control_epoch = dict(list(zip(self.loss_names, [1] * self.n_losses)))
 
+        self.last_loss_weights = {l: 0. for l in self.loss_names}
         self.backend_losses = {l: K.variable(0) for l in self.loss_names}
+        self.opt_states = None
+        self.optimizers = None
         self.update_loss_weights()
 
         pprint(vars(self))
@@ -420,8 +432,8 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
     def get_experiment_id(self):
         id = "{}_zdim{}_edim{}".format(self.name, self.z_dims, self.embedding_size)
         for l, k_loss in self.backend_losses.items():
-            id = "{}_L{}{}{}{}".format(id, l.replace('_', ''), self.loss_weights[l], 
-                self.loss_control[l].replace('-', ''), self.loss_control_epoch[l])
+            id = "{}_L{}{}{}{}".format(id, l.replace('_', ''), self.loss_weights[l],
+                                       self.loss_control[l].replace('-', ''), self.loss_control_epoch[l])
         return id.replace('.', '')
 
     train_on_batch = TOPGAN.__dict__['train_on_batch']
@@ -472,14 +484,21 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
 
     def update_loss_weights(self):
         log_message = "New loss weights: "
+        weight_delta = 0.
         for l, k_loss in self.backend_losses.items():
-            K.set_value(k_loss, change_losses_weight_over_time(self.current_epoch,
-                                                               self.loss_control_epoch[l],
-                                                               self.loss_control[l],
-                                                               self.loss_weights[l]))
+            new_loss = change_losses_weight_over_time(self.current_epoch,
+                                                      self.loss_control_epoch[l],
+                                                      self.loss_control[l],
+                                                      self.loss_weights[l])
+            K.set_value(k_loss, new_loss)
             log_message = "{}{}:{}, ".format(log_message, l, K.get_value(k_loss))
+            weight_delta = np.max((weight_delta, np.abs(new_loss - self.last_loss_weights[l])))
+
+        if weight_delta > 0.1:
+            self.last_loss_weights = {l: K.get_value(k_loss) for l, k_loss in self.backend_losses.items()}
+            self.reset_optimizers()
+            log_message = "{}. Did reset optmizers".format(log_message)
         print(log_message)
-        notify_with_message(log_message, experiment_id=self.experiment_id)
 
     def build_trainer(self):
         input_x = Input(shape=self.input_shape)
@@ -514,7 +533,7 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
         self.decoder.summary()
         self.aux_classifier.summary()
 
-        opt_d, opt_g = self.build_optmizers()
+        self.optimizers = self.build_optmizers()
         loss_d, loss_g, triplet_d_loss, triplet_g_loss = self.define_loss_functions()
 
         # build discriminator
@@ -522,7 +541,7 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
         set_trainable(self.f_Gx, False)
         set_trainable(self.f_D, not self.isolate_d_classifier)
         set_trainable(self.aux_classifier, True)
-        self.dis_trainer.compile(optimizer=opt_d,
+        self.dis_trainer.compile(optimizer=self.optimizers["opt_d"],
                                  loss=[loss_d, triplet_d_loss, 'binary_crossentropy'],
                                  loss_weights=[self.backend_losses['d_loss'], 0., 0.])
 
@@ -531,7 +550,7 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
         set_trainable(self.f_Gx, False)
         set_trainable(self.f_D, True)
         set_trainable(self.aux_classifier, not self.isolate_d_classifier)
-        self.ae_triplet_trainer.compile(optimizer=RMSprop(lr=self.lr*1e1),
+        self.ae_triplet_trainer.compile(optimizer=self.optimizers["opt_ae"],
                                         loss=[loss_d, triplet_d_loss, 'binary_crossentropy'],
                                         loss_weights=[0., self.backend_losses['d_triplet'], self.backend_losses['ae_loss']])
 
@@ -539,7 +558,7 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
         self.gen_trainer = self.build_trainer()
         set_trainable(self.f_Gx, True)
         set_trainable(self.f_D, False)
-        self.gen_trainer.compile(optimizer=opt_g,
+        self.gen_trainer.compile(optimizer=self.optimizers["opt_g"],
                                  loss=[loss_g, triplet_g_loss, 'binary_crossentropy'],
                                  loss_weights=[self.backend_losses['g_loss'], self.backend_losses['g_triplet'], 0.])
 
@@ -549,6 +568,24 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
         self.store_to_save('gen_trainer')
         self.store_to_save('f_Gx')
         self.store_to_save('f_D')
+
+        self.build_mmd_calc_model()
+
+    def build_optmizers(self):
+        opt_d = Adam(lr=self.lr)
+        opt_g = Adam(lr=self.lr)
+        opt_ae = Adam(lr=self.lr * 10)
+        return {"opt_d": opt_d,
+                "opt_g": opt_g,
+                "opt_ae": opt_ae}
+
+    def reset_optimizers(self):
+        if self.opt_states is None:  # never did that, initialize
+            if self.optimizers is not None:
+                self.opt_states = {k: opt.get_weights() for k, opt in self.optimizers.items()}
+            return
+        for k, opt in self.optimizers.items():
+            opt.set_weights(self.opt_states[k])
 
     def save_model(self, out_dir, epoch):
         self.trainers['f_Gx'] = self.f_Gx
@@ -564,29 +601,9 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
                      iterations_list=list(range(len(self.all_losses['ae_loss']))),
                      metric_names=[('g_loss', 'd_loss'), 'g_triplet', 'd_triplet', 'ae_loss'],
                      legend=[True, True, True, True],
+                     types=['lines'] * 4,
                      figsize=8,
-                     wspace=0.4)
-
-    def save_images(self, samples, filename, conditionals_for_samples=None):
-        '''
-        Save images generated from random sample numbers
-        '''
-        imgs = self.predict(samples)
-        np.random.seed(14)
-        perm = np.random.permutation(len(self.dataset))
-        imgs_from_dataset = self.dataset.images[perm[:10]]
-        noise = np.random.normal(scale=0.1, size=imgs_from_dataset.shape)
-        imgs_from_dataset += noise
-        np.random.seed()
-
-        imgs[80:90] = imgs_from_dataset
-        encoding = self.encoder.predict(imgs_from_dataset)
-        x_hat = self.decoder.predict(encoding)
-        imgs[90:] = x_hat
-        imgs = np.clip(imgs, 0., 1.)
-        if imgs.shape[3] == 1:
-            imgs = np.squeeze(imgs, axis=(3,))
-        self.save_image_as_plot(imgs, filename)
+                     wspace=0.15)
 
     def build_encoder(self):
         inputs = Input(shape=self.input_shape)
@@ -657,6 +674,14 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
 
         return Model(x_input, [x_embedding, discriminator, x_hat])
 
+    def make_predict_functions(self):
+        self.encoder._make_predict_function()
+        self.decoder._make_predict_function()
+        self.aux_classifier._make_predict_function()
+        self.f_D._make_predict_function()
+        self.f_Gx._make_predict_function()
+        self.mmd_model._make_test_function()
+
     def build_Gx(self):
         z_input = Input(shape=(self.z_dims,))
         orig_channels = self.input_shape[2]
@@ -680,56 +705,123 @@ class TOPGANwithAEfromEBGAN(BaseModel, metaclass=ABCMeta):
 
         return Model(z_input, x)
 
+    def build_mmd_calc_model(self):
+        input_z = Input(shape=(self.z_dims,))
+
+        x_hat = self.f_Gx(input_z)
+
+        self.mmd_model = Model(input_z, x_hat)
+        self.mmd_model.compile(optimizer='sgd', loss=mmd_lossfun)
+
+    """
+        Define all metrics that can be calculated here
+    """
+
     def calculate_svm_eval(self, finished_cgraph_use_event):
 
-        # get shuffled data
         np.random.seed(14)
         perm = np.random.permutation(len(self.dataset))
         x_test, y_test = self.dataset.images[perm[:1000]], np.argmax(self.dataset.attrs[perm[:1000]], axis=1)
-        x_train, y_train = self.dataset.images[perm[1000:]],  np.argmax(self.dataset.attrs[perm[1000:]], axis=1)
+        x_train, y_train = self.dataset.images[perm[1000:6000]],  np.argmax(self.dataset.attrs[perm[1000:6000]], axis=1)
         np.random.seed()
 
         feats = self.encoder.predict(x_train)
         feats_test = self.encoder.predict(x_test)
         finished_cgraph_use_event.set()
 
-        # rescale data
-        scaler = StandardScaler()
-        feats = scaler.fit_transform(feats)
-        feats_test = scaler.transform(feats_test)
+        return svm_eval(feats, y_train, feats_test, y_test)
+    svm_eval_metric_type = 'lines'
 
-        # grid search is performed on those C values
-        param_grid = [{'C': [1e1]}]
+    def calculate_tsne(self, finished_cgraph_use_event):
 
-        # get 10 stratified shuffle splits of 1000 samples (stratified meaning it
-        # keeps the class distribution intact).
-        dataset_splits = StratifiedShuffleSplit(
-            n_splits=2, train_size=1000, test_size=0.2).split(feats, y_train)
+        np.random.seed(14)
+        perm = np.random.permutation(len(self.dataset))
+        x_test, y_test = self.dataset.images[perm[:1000]], np.argmax(self.dataset.attrs[perm[:1000]], axis=1)
+        np.random.seed()
 
-        # perform grid search on each different split and get best linear SVM
-        scores = []
-        for split in dataset_splits:
-            grid = GridSearchCV(LinearSVC(),
-                                param_grid=param_grid,
-                                cv=[split], n_jobs=1)
-            grid.fit(feats, y_train)
-            score_on_test = grid.score(feats_test, y_test)
-            scores.append(score_on_test)
+        feats_test = self.encoder.predict(x_test)
+        finished_cgraph_use_event.set()
 
-        mean = np.mean(scores)
-        return mean
+        return tsne(feats_test, y_test)
+    tsne_metric_type = 'scatter'
 
+    def calculate_lda(self, finished_cgraph_use_event):
+
+        np.random.seed(14)
+        perm = np.random.permutation(len(self.dataset))
+        x_test, y_test = self.dataset.images[perm[:1000]], np.argmax(self.dataset.attrs[perm[:1000]], axis=1)
+        np.random.seed()
+
+        feats_test = self.encoder.predict(x_test)
+        finished_cgraph_use_event.set()
+
+        return lda(feats_test, y_test)
+    lda_metric_type = 'scatter'
+
+    def calculate_pca(self, finished_cgraph_use_event):
+
+        np.random.seed(14)
+        perm = np.random.permutation(len(self.dataset))
+        x_test, y_test = self.dataset.images[perm[:1000]], np.argmax(self.dataset.attrs[perm[:1000]], axis=1)
+        np.random.seed()
+
+        feats_test = self.encoder.predict(x_test)
+        finished_cgraph_use_event.set()
+
+        return pca(feats_test, y_test)
+    pca_metric_type = 'scatter'
+
+    def calculate_samples(self, finished_cgraph_use_event):
+        np.random.seed(14)
+        samples = np.random.normal(size=(36, self.z_dims))
+        np.random.seed()
+        imgs = self.f_Gx.predict(samples)
+        if imgs.shape[3] == 1:
+            imgs = np.squeeze(imgs, axis=(3,))
+        finished_cgraph_use_event.set()
+        return imgs
+    samples_metric_type = 'image-grid'
+
+    def calculate_ae_rec(self, finished_cgraph_use_event):
+        np.random.seed(14)
+        perm = np.random.permutation(len(self.dataset))
+        imgs_from_dataset = self.dataset.images[perm[:18]]
+        noise = np.random.normal(scale=0.1, size=imgs_from_dataset.shape)
+        imgs_from_dataset += noise
+        np.random.seed()
+
+        imgs = np.zeros((36,) + self.input_shape)
+        encoding = self.encoder.predict(imgs_from_dataset)
+        x_hat = self.decoder.predict(encoding)
+        imgs[0::2] = imgs_from_dataset
+        imgs[1::2] = x_hat
+        imgs = np.clip(imgs, 0., 1.)
+        if imgs.shape[3] == 1:
+            imgs = np.squeeze(imgs, axis=(3,))
+        finished_cgraph_use_event.set()
+        return imgs
+    ae_rec_metric_type = 'image-grid'
+
+    def calculate_mmd(self, finished_cgraph_use_event):
+        np.random.seed(14)
+        perm = np.random.permutation(len(self.dataset))
+        samples = np.random.normal(size=(5000, self.z_dims))
+        np.random.seed()
+
+        imgs_from_dataset = self.dataset.images[perm[:5000]]
+        mmd = self.mmd_model.evaluate(samples, imgs_from_dataset, verbose=0, batch_size=5000)
+        finished_cgraph_use_event.set()
+        print(mmd)
+        return mmd
+    mmd_metric_type = 'lines'
 
 class TOPGANwithAEforMNIST(TOPGANwithAEfromEBGAN):
 
     def build_encoder(self):
         inputs = Input(shape=self.input_shape)
 
-        x = BasicConvLayer(128, (5, 5), strides=(2, 2), bnorm=False, activation='relu')(inputs)
-        x = BasicConvLayer(128, (3, 3), strides=(1, 1), bnorm=True, activation='relu')(x)
-        x = BasicConvLayer(128, (4, 4), strides=(2, 2), bnorm=True, activation='relu')(x)
+        x = BasicConvLayer(64, (4, 4), strides=(2, 2), bnorm=False, activation='relu')(inputs)
         x = Flatten()(x)
-
         x = Dense(self.embedding_size)(x)
         x = Activation('linear')(x)
 
@@ -739,20 +831,13 @@ class TOPGANwithAEforMNIST(TOPGANwithAEfromEBGAN):
         z_input = Input(shape=(self.embedding_size,))
         orig_channels = self.input_shape[2]
 
-        w = self.input_shape[0] // 2**2
-        c = self.embedding_size // (w * w)
-        try:
-            x = Reshape((w, w, c))(z_input)
-        except ValueError:
-            raise ValueError("The embedding size must be divisible by {}*{}"
-                             " for input shape {}".format(w, w, self.input_shape))
+        w = self.input_shape[0] // 2**1  # starting width
+        x = Dense(64 * w * w)(z_input)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Reshape((w, w, 64))(x)
 
-        x = BasicDeconvLayer(128, (4, 4), strides=(2, 2), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1, padding='same')(x)
-        x = BasicDeconvLayer(128, (3, 3), strides=(1, 1), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1, padding='same')(x)
-        x = BasicDeconvLayer(128, (4, 4), strides=(2, 2), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1, padding='same')(x)
-
-        x = BasicConvLayer(128, (1, 1), strides=(1, 1), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1)(x)
-        x = BasicConvLayer(orig_channels, (1, 1), activation='sigmoid', bnorm=False)(x)
+        x = BasicDeconvLayer(orig_channels, (4, 4), strides=(2, 2), bnorm=False, activation='sigmoid', padding='same')(x)
 
         return Model(z_input, x)
 
@@ -761,10 +846,10 @@ class TOPGANwithAEforMNIST(TOPGANwithAEfromEBGAN):
 
         x = Activation('relu')(embedding_input)
         x = BatchNormalization()(x)
-        x = Dense(512)(x)
+        x = Dense(64)(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
-        x = Dense(512)(x)
+        x = Dense(64)(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
         x = Dense(1)(x)
@@ -776,19 +861,16 @@ class TOPGANwithAEforMNIST(TOPGANwithAEfromEBGAN):
         z_input = Input(shape=(self.z_dims,))
         orig_channels = self.input_shape[2]
 
-        w = self.input_shape[0] // 2**2
-        c = self.z_dims // (w * w)
-        try:
-            x = Reshape((w, w, c))(z_input)
-        except ValueError:
-            raise ValueError("Latent space dims must be divisible by {}*{}"
-                             " for input shape {}".format(w, w, self.input_shape))
+        w = self.input_shape[0] // 2**2  # starting width
+        x = Dense(1024)(z_input)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Dense(128 * w * w)(z_input)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Reshape((w, w, 128))(x)
 
-        x = BasicDeconvLayer(128, (4, 4), strides=(2, 2), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1, padding='same')(x)
-        x = BasicDeconvLayer(128, (3, 3), strides=(1, 1), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1, padding='same')(x)
-        x = BasicDeconvLayer(128, (4, 4), strides=(2, 2), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1, padding='same')(x)
-
-        x = BasicConvLayer(128, (1, 1), strides=(1, 1), bnorm=True, activation='leaky_relu', leaky_relu_slope=0.1)(x)
-        x = BasicConvLayer(orig_channels, (1, 1), activation='sigmoid', bnorm=False)(x)
+        x = BasicDeconvLayer(64, (4, 4), strides=(2, 2), bnorm=True, activation='relu', padding='same')(x)
+        x = BasicDeconvLayer(orig_channels, (4, 4), strides=(2, 2), bnorm=False, activation='sigmoid', padding='same')(x)
 
         return Model(z_input, x)

models/utils.py
@@ -1,19 +1,25 @@
 from keras import backend as K
 from keras.callbacks import Callback
-import os, sys
+import os
+import sys
 import numpy as np
-from matplotlib.gridspec import GridSpec
+from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
 from matplotlib.ticker import FormatStrFormatter
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from matplotlib import ticker
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+
 
 def set_trainable(model, train):
     """
     Enable or disable training for the model
     """
-    if type(model) != list: mlist = [model]
-    else: mlist = model
+    if type(model) != list:
+        mlist = [model]
+    else:
+        mlist = model
     for model in mlist:
         model.trainable = train
         for l in model.layers:
@@ -47,6 +53,7 @@ def interpolate_vectors(a, b, steps=10):
 
 
 class PlotReconstructions(Callback):
+
     def __init__(self, encoder, decoder, z_dim=256, examples_to_plot=10):
         super().__init__()
         self.encoder = encoder
@@ -123,6 +130,7 @@ def get_gradient_norm_func(model):
     func = K.function(inputs=input_tensors, outputs=[norm])
     return func
 
+
 def time_format(t):
     m, s = divmod(t, 60)
     m = int(m)
@@ -131,6 +139,7 @@ def time_format(t):
         return '%d sec' % s
     else:
         return '%d min %d sec' % (m, s)
+
 
 def print_current_progress(current_epoch, current_batch_index, batch_size, dataset_length, losses, elapsed_time):
     ratio = 100.0 * (current_batch_index + batch_size) / dataset_length
@@ -146,77 +155,88 @@ def print_current_progress(current_epoch, current_batch_index, batch_size, datas
     print(status_string)
     sys.stdout.flush()
 
+
 def add_input_noise(x_batch, curr_epoch, total_epochs, start_noise):
     if start_noise == 0.0:
         return x_batch
 
-    if type(x_batch) == tuple: batchsize = x_batch[0].shape
-    else: batchsize = x_batch.shape
+    if type(x_batch) == tuple:
+        batchsize = x_batch[0].shape
+    else:
+        batchsize = x_batch.shape
     noise = np.random.normal(size=batchsize)
 
     noise_factor = curr_epoch / total_epochs
     if noise_factor < 0.02:
         return x_batch
 
-    if type(x_batch) == tuple: noised_batch = tuple([X + noise * noise_factor for X in x_batch])
-    else: noised_batch = x_batch + noise * noise_factor
+    if type(x_batch) == tuple:
+        noised_batch = tuple([X + noise * noise_factor for X in x_batch])
+    else:
+        noised_batch = x_batch + noise * noise_factor
     return noised_batch
+
 
 def add_input_noise_to_autoencoder(x_batch):
     batchsize = x_batch.shape
     noise = np.random.normal(size=batchsize)
-    return x_batch + noise*0.0
+    return x_batch + noise * 0.0
+
 
 def smooth_binary_labels(batchsize, smoothing=0.0, one_sided_smoothing=True):
-        if smoothing > 0.0:
-            y_pos = 1. - np.random.random((batchsize, )) * smoothing
-            if one_sided_smoothing:
-                y_neg = np.zeros(batchsize, dtype='float32')
-            else:
-                y_neg = np.random.random((batchsize, )) * smoothing
-        else:
-            y_pos = np.ones(batchsize, dtype='float32')
+    if smoothing > 0.0:
+        y_pos = 1. - np.random.random((batchsize, )) * smoothing
+        if one_sided_smoothing:
             y_neg = np.zeros(batchsize, dtype='float32')
+        else:
+            y_neg = np.random.random((batchsize, )) * smoothing
+    else:
+        y_pos = np.ones(batchsize, dtype='float32')
+        y_neg = np.zeros(batchsize, dtype='float32')
 
-        return y_pos, y_neg
+    return y_pos, y_neg
 
-def plot_metrics(outfile, metrics_list, iterations_list,
-                     metric_names=None, n_cols=2, legend=False, x_label=None,
-                     y_label=None, wspace=None, hspace=None, figsize=8):
-        # cmap=plt.cm.tab20
-        assert isinstance(metrics_list, (list, tuple)) and \
-            not isinstance(metrics_list, str)
 
-        if len(metrics_list) == 1:
-            grid_cols, grid_rows = 1, 1
-        elif len(metrics_list) == 2:
-            grid_cols, grid_rows = 2, 1
-        elif len(metrics_list) == 3 or len(metrics_list) == 4:
-            grid_cols, grid_rows = 2, 2
-        elif len(metrics_list) == 5 or len(metrics_list) == 6:
-            grid_cols, grid_rows = 2, 3
-        elif len(metrics_list) == 7 or len(metrics_list) == 8 or len(metrics_list) == 9:
-            grid_cols, grid_rows = 3, 3
+def plot_metrics(outfile, metrics_list, iterations_list, types,
+                 metric_names=None, n_cols=2, legend=False, x_label=None,
+                 y_label=None, wspace=None, hspace=None, figsize=8):
 
-        fig_w, fig_h = figsize*grid_cols, figsize*grid_cols
+    assert isinstance(metrics_list, (list, tuple)) and \
+        not isinstance(metrics_list, str)
 
-        fig = plt.figure(figsize=(fig_w, fig_h))
+    total_n_plots = len(metrics_list)
+    if total_n_plots == 1:
+        grid_cols, grid_rows = 1, 1
+    elif total_n_plots == 2:
+        grid_cols, grid_rows = 2, 1
+    elif total_n_plots == 3 or total_n_plots == 4:
+        grid_cols, grid_rows = 2, 2
+    elif total_n_plots == 5 or total_n_plots == 6:
+        grid_cols, grid_rows = 2, 3
+    elif total_n_plots == 7 or total_n_plots == 8 or total_n_plots == 9:
+        grid_cols, grid_rows = 3, 3
 
-        gs = GridSpec(grid_rows, grid_cols)
-        if wspace is not None and hspace is not None:
-            gs.update(wspace=wspace, hspace=hspace)
-        elif wspace is not None:
-            gs.update(wspace=wspace)
-        elif hspace is not None:
-            gs.update(hspace=hspace)
+    fig_w, fig_h = figsize * grid_cols, figsize * grid_rows
 
-        for ii, metric in enumerate(metrics_list):
+    fig = plt.figure(figsize=(fig_w, fig_h))
 
-            ax = plt.subplot(gs[ii // grid_cols, ii % grid_cols])
+    gs = GridSpec(grid_rows, grid_cols)
+    if wspace is not None and hspace is not None:
+        gs.update(wspace=wspace, hspace=hspace)
+    elif wspace is not None:
+        gs.update(wspace=wspace)
+    elif hspace is not None:
+        gs.update(hspace=hspace)
 
-            ax.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
+    for ii, metric in enumerate(metrics_list):
 
-            if isinstance(metric[0], (list, tuple)):
+        current_cell = gs[ii // grid_cols, ii % grid_cols]
+        ax = None
+
+        if types[ii] == 'lines':
+            ax = plt.subplot(current_cell)
+            ax.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
+            if isinstance(metric[0], (list, tuple, np.ndarray)):
                 lines = []
                 for jj, submetric in enumerate(metric):
                     if metric_names is not None:
@@ -235,19 +255,46 @@ def plot_metrics(outfile, metrics_list, iterations_list,
                 line, = ax.plot(iterations_list, metric, color='C0',
                                 label=label)
                 lines = [line]
+            if ((not isinstance(legend, (list, tuple)) and legend) or
+                    (isinstance(legend, (list, tuple)) and legend[ii])):
+                lg = ax.legend(handles=lines)
 
-            if (not isinstance(legend, (list, tuple)) and legend) or \
-                    (isinstance(legend, (list, tuple)) and legend[ii]):
-                lg = ax.legend(handles=lines,
-                               bbox_to_anchor=(1.0, 1.0),
-                               loc="upper left")
-                bbox_extra_artists = (lg, )
-            else:
-                bbox_extra_artists = None
+        elif types[ii] == 'scatter':
+            ax = plt.subplot(current_cell)
+            ax.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
+            cmap = cm.tab10
+            category_labels = metric[-1][..., 2]
+            norm = colors.Normalize(vmin=np.min(category_labels), vmax=np.max(category_labels))
+            cmapper = cm.ScalarMappable(norm=norm, cmap=cmap)
+            mapped_colors = cmapper.to_rgba(category_labels)
+            unique_labels = list(set(category_labels))
+            lines = ax.scatter(metric[-1][..., 0], metric[-1][..., 1],
+                               color=mapped_colors,
+                               label=unique_labels)
+            ax.text(0.1, 0.95, metric_names[ii],
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    transform=ax.transAxes,
+                    fontsize=14)
 
+        elif types[ii] == 'image-grid':
+            imgs = metric[-1]
+            n_images = len(imgs)
+            inner_grid_width = int(np.sqrt(n_images))
+            inner_grid = GridSpecFromSubplotSpec(inner_grid_width, inner_grid_width, current_cell, wspace=0.1, hspace=0.1)
+            for i in range(n_images):
+                inner_ax = plt.subplot(inner_grid[i])
+                if imgs.ndim == 4:
+                    inner_ax.imshow(imgs[i, :, :, :], interpolation='none', vmin=0.0, vmax=1.0)
+                else:
+                    inner_ax.imshow(imgs[i, :, :], cmap='gray', interpolation='none', vmin=0.0, vmax=1.0)
+                inner_ax.axis('off')
+
+        
+        if ax is not None:
             if x_label is not None and not isinstance(x_label, (list, tuple)):
                 ax.set_xlabel(x_label, color='k')
-            elif isinstance(x_label, (list, tuple)):
+            elif isinstance(x_label, (list, tuple)) and ax is not None:
                 ax.set_xlabel(x_label[ii], color='k')
 
             # Make the y-axis label, ticks and tick labels
@@ -258,17 +305,16 @@ def plot_metrics(outfile, metrics_list, iterations_list,
                 ax.set_ylabel(y_label[ii], color='k')
             ax.tick_params('y', colors='k')
 
-        plt.savefig(
-            outfile, dpi=300,
-            bbox_extra_artists=bbox_extra_artists, bbox_inches='tight')
-        plt.close(fig)
+    plt.savefig(outfile, dpi=150, bbox_inches='tight')
+    plt.close(fig)
+
 
 def change_losses_weight_over_time(current_epoch, max_epoch, type, scalar_loss):
-    weighting_factor = current_epoch/max_epoch
+    weighting_factor = current_epoch / max_epoch
     if type == 'inc':
-        return scalar_loss*weighting_factor
+        return scalar_loss * weighting_factor
     elif type == 'dec':
-        return scalar_loss*(1-weighting_factor)
+        return scalar_loss * (1 - weighting_factor)
     elif type == 'hold':
         if current_epoch >= max_epoch:
             return scalar_loss
@@ -276,14 +322,14 @@ def change_losses_weight_over_time(current_epoch, max_epoch, type, scalar_loss):
             return 0.0
     elif type == 'hold-inc':
         if current_epoch >= max_epoch:
-            weighting_factor = (current_epoch-max_epoch)/max_epoch
-            return scalar_loss*weighting_factor
+            weighting_factor = (current_epoch - max_epoch) / max_epoch
+            return scalar_loss * weighting_factor
         else:
             return 0.0
     elif type == 'hold-dec':
         if current_epoch >= max_epoch:
-            weighting_factor = (current_epoch-max_epoch)/max_epoch
-            return scalar_loss*(1-weighting_factor)
+            weighting_factor = (current_epoch - max_epoch) / max_epoch
+            return scalar_loss * (1 - weighting_factor)
         else:
             return 0.0
     elif type == 'halt':